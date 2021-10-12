3bd8038a630425ac820ad0dbab0a3f39c71191de
Sanket Mundada <mundadasanket2000@gmail.com>
2021-10-12 13:59:51 +0530

    PR raised

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const App: React.FC = () => {
-  return <div>Hello hello</div>;
+  return <div>Hello World</div>;
 };
 
 export default App;
